2dc8c0f130e7d0e43879151bd3ea726a31d3ee5e
Jesse Futerman <jessefuterman@gmail.com>
2019-07-05 12:55:51 -0400

    july 5 I FIXED IT FOR REAL THIS TIME

--- src/component/ProgressBarExample.js
+++ src/component/ProgressBarExample.js
@@ -5,24 +5,13 @@ class ProgressBarExample extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      percentage: 10
-    };
+      percentage: 0
+    }
   }
 
   
 
-  Filler = () => {
-    
-    
-    return <div className="filler" style={{ width:`${this.state.percentage}%` }} />;
-  }; 
 
-
-JuiceFill = () => {
-  
-  
-
-}
 
  
   
@@ -38,11 +27,12 @@ JuiceFill = () => {
     return (
         
         <div>
-        
+       
         <div className="progress-bar">
-        <this.Filler>  </this.Filler>
+        <div className="filler" style={{ width:`${this.state.percentage}%` }} />
       </div>
       <div>
+      
       </div>
         </div>
 
