0935576a5ac758f75ba76a19ea5d3b5014b5cabf
Jesse Futerman <jessefuterman@gmail.com>
2019-08-12 15:02:47 -0400

    aug12 adding log in and pass

--- src/App.js
+++ src/App.js
@@ -4,29 +4,47 @@ import "./App.css";
 import Header from "./component/Header.js";
 import Input from "./component/Input.js";
 import List from "./component/List.js";
-// import fire from "./component/fire.js";
+import fire from "./component/fire.js";
+import Login from "./component/Login.js";
+
 // import firebase from "firebase"
-
-
-
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       items: [],
-      user: {},
+      user: null
     };
+    this.authListener = this.authListener.bind(this);
   }
 
-  
+  componentDidMount() {
+    this.authListener();
+  }
+
+  logout = () => {
+    fire.auth().signOut();
+}
+  authListener() {
+    fire.auth().onAuthStateChanged(user => {
+      console.log(user);
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem("user", user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+      }
+    });
+  }
 
   render() {
     return (
       <div className="App">
-     
-      <Header></Header>
-        <Input />
+        <Header />
+        <div>{this.state.user ? <Input /> : <Login />}</div>
+        <button onClick={this.logout}>Logout</button>
       </div>
     );
   }
--- src/component/fire.js
+++ src/component/fire.js
@@ -1,26 +1,13 @@
-import firebase from "firebase"
+import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1SJqzufUyUagLYOjUaMvXQPVa8BiJSsM",
-    authDomain: "swoleon-b9f8c.firebaseapp.com",
-    databaseURL: "https://swoleon-b9f8c.firebaseio.com",
-    projectId: "swoleon-b9f8c",
+    apiKey: "AIzaSyA8oDyout1MEqwn9r8BHgF9KsU1HbitSxg",
+    authDomain: "fir-project1-deb4e.firebaseapp.com",
+    databaseURL: "https://fir-project1-deb4e.firebaseio.com",
+    projectId: "fir-project1-deb4e",
     storageBucket: "",
-    messagingSenderId: "716241455226",
-    appId: "1:716241455226:web:fc894bd8be45e196"
+    messagingSenderId: "746453714210",
+    appId: "1:746453714210:web:e8f888ec64763fe2"
   };
-  // Initialize Firebase
-  
-  const fire =  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database()
-  var ref = database.ref("scores")
-
-  var data = {
-      name:"Jesse",
-      score: "20"
-  }
-  ref.push(data)
-
-
-
-  export default fire
+  const fire = firebase.initializeApp(firebaseConfig);
+export default fire;
